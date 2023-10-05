fabe4e15bb452b766fd91fd398ba868d3f2ec4ad
Anssi Rauhala <anssi.rauhala1@gmail.com>
2023-10-05 20:51:52 +0300

    harj6 1 pojo

--- harj6.py
+++ harj6.py
@@ -13,34 +13,54 @@ canvas = tk.Canvas(ikkuna, width=600, height=600, bg='blue')
 canvas.place(x=50, y=50)
 
 # add an island to the center of canvas the size of 400x400
-island = canvas.create_rectangle(100, 100, 500, 500, fill='green')
-
-# add a swimming pool to the center of hte island the size of 60x20
+island = canvas.create_rectangle(100, 100, 500, 500, fill='burlywood1')
 
 
+ernesti_oja = np.zeros((10, 1))
 
-# matetmatiikkaa...
-oja = np.zeros((10, 1))
-for i in range(10):
-    oja[i] = 5
+ernesti_oja_fig, ernesti_oja_ax = plt.subplots(figsize=(0.2, 2))
+ernesti_oja_fig.set_facecolor("none")
+ernesti_oja_ax.matshow(ernesti_oja)
+ernesti_oja_ax.axis("off")
+ernesti_oja_fig.tight_layout(pad=0)
+
+ernesti_oja_canvas = FigureCanvasTkAgg(ernesti_oja_fig, master=ikkuna)
+ernesti_oja_canvas.get_tk_widget().place(x=250, y=150)
+
+# fill ernesti_oja with 1's
+for i in range(len(ernesti_oja)):
+    ernesti_oja_ax.text(0, i, 1, va="center", ha="center",
+                        color="white", fontsize=10)
 
 
-# perinteiseen tapaan ... piirretään kuvaaja "erilleen"...
-fig1, ax1 = plt.subplots(figsize=(0.2, 2))
-fig1.set_facecolor("#F0F0F0")
-ax1.matshow(oja)
-ax1.axis("off")
-# remove padding around the figure
-plt.tight_layout(pad=0)
+kernesti_oja = np.zeros((10, 1))
 
+kernesti_oja_fig, kernesti_oja_ax = plt.subplots(figsize=(0.2, 2))
+kernesti_oja_fig.set_facecolor("none")
+kernesti_oja_ax.matshow(kernesti_oja)
+kernesti_oja_ax.axis("off")
+kernesti_oja_fig.tight_layout(pad=0)
 
+kernesti_oja_canvas = FigureCanvasTkAgg(kernesti_oja_fig, master=ikkuna)
+kernesti_oja_canvas.get_tk_widget().place(x=430, y=150)
 
+for i in range(len(kernesti_oja)):
+    kernesti_oja_ax.text(0, i, 1, va="center", ha="center",
+                         color="white", fontsize=10)
 
-# erikoisella tavalla...kuvaajan pitää näkyä käyttöliittymässä...
-erikois_oja_kanvaasi = FigureCanvasTkAgg(fig1, master=ikkuna)
-erikois_oja_kanvaasi.get_tk_widget().place(x=250, y=150)
+pool = np.zeros((2, 6))
+pool_fig, pool_ax = plt.subplots(figsize=(2, 0.67))
+pool_ax.matshow(pool, cmap="viridis")
+pool_ax.axis("off")
+pool_fig.tight_layout(pad=0)
+pool_fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
+pool_in_canvas = FigureCanvasTkAgg(pool_fig, master=ikkuna)
+pool_in_canvas.get_tk_widget().place(x=250, y=350)
 
-erikois_oja_kanvaasi.draw()
+for i in range(len(pool)):
+    for j in range(len(pool[i])):
+        pool_ax.text(j, i, int(pool[i][j]), va="center",
+                     ha="center", color="white", fontsize=10)
 
 
 # add five buttons to the top line of the window
